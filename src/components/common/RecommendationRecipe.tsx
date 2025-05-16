4f537b84b10d9aff43a72b3ab35973a9da9d93c4
kavana77 <kavanab77@gmail.com>
2025-05-16 17:48:39 +0530

    made changes

--- src/components/common/RecommendationRecipe.tsx
+++ src/components/common/RecommendationRecipe.tsx
@@ -1,6 +1,6 @@
 import Text from "../ui/Text";
 import Clock from "../../assets/recommendationrecipe/clock.svg";
-import ForkKnife from "../../assets/recommendationrecipe/forkKnife.svg";
+import ForkKnife from "../../assets/recommendationrecipe/forkknife.svg";
 import { rrecipes } from "@/utils/constant";
 
 const RecommendationRecipe: React.FC = () => {
