b75c9273753036241d7bef259b08826c571e0c1f
kavana77 <kavanab77@gmail.com>
2025-06-02 19:26:31 +0530

    structure changes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,36 @@
+import { useRef,useState, type FormEvent } from "react";
+import emailjs from "@emailjs/browser";
 import Text from "./ui/Text";
 import Input from "./ui/Input";
 import Image from "@/assets/images/contact.png";
 const Contact = () => {
-  function handleSubmit() {}
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const form = useRef<HTMLFormElement>(null);
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        form.current as HTMLFormElement,
+        {
+          publicKey: import.meta.env.VITE_PUBLIC_KEY,
+        }
+      )
+      .then(
+        () => {
+          setSuccess(true);
+          setError(false);
+        },
+        (error) => {
+          console.log(error);
+          setError(true);
+          setSuccess(false);
+        }
+      );
+  };
+
   return (
     <div className="w-full min-h-screen px-4 sm:px-8 lg:px-14 mt-16">
       <Text variant="heading" className="text-center mb-7">
@@ -19,7 +47,7 @@ const Contact = () => {
         </div>
 
         {/* Form Section */}
-        <form className="w-full space-y-6 bg-white">
+        <form className="w-full space-y-6 bg-white" ref={form} onSubmit={sendEmail}>
           {/* Row 1: Name & Email */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <Input
@@ -75,11 +103,15 @@ const Contact = () => {
             <button
               type="submit"
               className="bg-black text-white px-6 py-3 rounded-md hover:bg-gray-800 transition"
-              onClick={handleSubmit}
+             
             >
               Submit
             </button>
           </div>
+          {success && (
+        <span className="text-green-400">Your message has been sent!</span>
+      )}
+      {error && <span className="text-red-400">Something went wrong!</span>}
         </form>
       </div>
     </div>
